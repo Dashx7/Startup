f3f93fc972cd0ad4db62b37f5753aae5b98b975d
DashX7 <josh.a.wiseman@gmail.com>
2023-11-19 14:05:06 -0700

    Test 6 working?

--- Public/database.js
+++ Public/database.js
@@ -1,4 +1,4 @@
-// Purpose: Database connection
+// Purpose: Database connection and configuration
 const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
