0fa72b3e11545ebed094387bfa17430c4ceb9e69
DashX7 <josh.a.wiseman@gmail.com>
2023-11-19 13:49:46 -0700

    Making sure gitignore works

--- Index.js
+++ Index.js
@@ -68,3 +68,39 @@ function updateWorkouts(newWorkout, workouts) {
     console.log("From the API call",workouts);
     return workouts;
 }
+
+//MONGODB section
+const { MongoClient } = require('mongodb');
+
+//TODO change this to the actual database
+const userName = 'holowaychuk';
+const password = 'express';
+const hostname = 'mongodb.com';
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+
+// await client.connect(); // Connect to the MongoDB cluster
+
+const collection = client.db('rental').collection('house');
+
+const house = {
+  name: 'Beachfront views',
+  summary: 'From your bedroom to the beach, no shoes required',
+  property_type: 'Condo',
+  beds: 1,
+};
+await collection.insertOne(house);
+
+
+const query = { property_type: 'Condo', beds: { $lt: 2 } };
+
+const options = {
+  sort: { price: -1 },
+  limit: 10,
+};
+
+const cursor = collection.find(query, options);
+const rentals = await cursor.toArray();
+rentals.forEach((i) => console.log(i));
