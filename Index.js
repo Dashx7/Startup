495fc36374564f8c59d26042080009140fb14115
DashX7 <josh.a.wiseman@gmail.com>
2023-11-22 15:11:09 -0700

    Fixed a mistake

--- Index.js
+++ Index.js
@@ -69,50 +69,12 @@ function updateWorkouts(newWorkout, workouts) {
     return workouts;
 }
 
-//MONGODB section
-// const { MongoClient } = require('mongodb');
-
-// //TODO change this to the actual database
-// const userName = 'holowaychuk';
-// const password = 'express';
-// const hostname = 'mongodb.com';
-
-// const url = `mongodb+srv://${userName}:${password}@${hostname}`;
-
-// const client = new MongoClient(url);
-
-// // await client.connect(); // Connect to the MongoDB cluster
-
-// const collection = client.db('rental').collection('house');
-
-// const house = {
-//   name: 'Beachfront views',
-//   summary: 'From your bedroom to the beach, no shoes required',
-//   property_type: 'Condo',
-//   beds: 1,
-// };
-// await collection.insertOne(house);
-
-
-// const query = { property_type: 'Condo', beds: { $lt: 2 } };
-
-// const options = {
-//   sort: { price: -1 },
-//   limit: 10,
-// };
-
-// const cursor = collection.find(query, options);
-// const rentals = await cursor.toArray();
-// rentals.forEach((i) => console.log(i));
-
-
-//MongoDB Test
+//MONGO DB Section
 const { MongoClient } = require('mongodb');
 const config = require('./public/dbconfig.json') //Updated to the public folder
 
 async function main() {
     // Connect to the database cluster
-    //   const url = "mongodb+srv://JoshWiseman:Mj20032003@joshwisemancluster.yfd6qrz.mongodb.net/?retryWrites=true&w=majority";
     const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
     const client = new MongoClient(url);
@@ -150,28 +112,3 @@ async function main() {
 }
 
 main().catch(console.error);
-
-//MongoDB Test 2, from their website
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb://atlas-sql-654932bced4bab67ba4c9f27-wck9s.a.query.mongodb.net/Gym_Bro_Buddy?ssl=true&authSource=admin"
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
